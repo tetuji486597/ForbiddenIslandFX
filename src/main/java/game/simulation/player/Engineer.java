f4c7da13bebdc160da696b871cfc244dcb42d817
yuiy0311 <luckykitty0311@gmail.com>
2021-11-19 12:18:13 -0600

    edit engineeer getMoveableTiles

--- src/main/java/game/simulation/player/Engineer.java
+++ src/main/java/game/simulation/player/Engineer.java
@@ -11,10 +11,32 @@ public class Engineer {
 
     public Engineer(){//
         // moveableTiles = new boolean[GameState.tiles.length];
+        moveableTiles = new boolean[3][3];
         }
 
     public boolean[][] getMoveableTiles(GameTile tile){
        int [] pos = tile.getPosition();
+       int i = 0;
+        for(int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 3; c++)
+            {
+
+                if(r == 1 && c == 1 || GameState.tiles[i].isGone())
+                {
+                    moveableTiles[r][c] = false;
+                }
+
+                else
+                {
+                    moveableTiles[r][c] = true;
+                }
+                i++;
+            }
+        }
+        return moveableTiles;
+
+       /*
         for(int i = 0; i<GameState.tiles.length; i++){
             int[] temp = GameState.tiles[i].getPosition();
             if(GameState.tiles[i].isGone()){
@@ -30,7 +52,6 @@ public class Engineer {
 
         }
 
-
-        return moveableTiles;
+*/
     }
 }
