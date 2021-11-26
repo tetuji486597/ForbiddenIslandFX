0a1a0bccaff7c0f9bbdb4ff751ee774371834d35
yuiy0311 <luckykitty0311@gmail.com>
2021-11-26 17:26:35 -0600

    navigator getMoveableTiles and getOtherMoveableTiles

--- src/main/java/game/simulation/player/Navigator.java
+++ src/main/java/game/simulation/player/Navigator.java
@@ -1,10 +1,66 @@
 package game.simulation.player;
 
+import game.simulation.board.GameTile;
+import game.simulation.brains.GameState;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Navigator extends Player{
+    private boolean[][] moveableTiles;
+    private boolean[][] otherMoveableTiles;
+
     public Navigator(String role, ArrayList<String> startingDeck) throws FileNotFoundException {
         super(role,startingDeck);
+        moveableTiles = new boolean[3][3];
+        otherMoveableTiles = new boolean [5][5];
+    }
+
+    public boolean[][] getMoveableTiles(GameTile tile){
+        int [] pos = tile.getPosition();
+        int i = 0;
+        for(int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 3; c++)
+            {
+
+                if(r == 1 && c == 1 || GameState.tiles[i].isGone())
+                {
+                    moveableTiles[r][c] = false;
+                }
+
+                else
+                {
+                    moveableTiles[r][c] = true;
+                }
+                i++;
+            }
+        }
+        return moveableTiles;
+    }
+
+
+    public boolean[][] getOtherMoveableTiles(GameTile tile) {
+        int [] pos = tile.getPosition();
+        int i = 0;
+        for(int r = 0; r < 5; r++)
+        {
+            for(int c = 0; c < 5; c++)
+            {
+
+                if(r == 3 && c == 3 || GameState.tiles[i].isGone())
+                {
+                    moveableTiles[r][c] = false;
+                }
+
+                else
+                {
+                    moveableTiles[r][c] = true;
+                }
+                i++;
+            }
+        }
+
+         return otherMoveableTiles;
     }
 }
