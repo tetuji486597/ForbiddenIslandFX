57c0727597e16f195f29c811b2b14178c2cbadc1
yuiy0311 <luckykitty0311@gmail.com>
2021-11-26 17:10:56 -0600

    pilot getMoveableTiles

--- src/main/java/game/simulation/player/Pilot.java
+++ src/main/java/game/simulation/player/Pilot.java
@@ -1,10 +1,29 @@
 package game.simulation.player;
 
+import game.simulation.board.GameTile;
+import game.simulation.brains.GameState;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Pilot extends Player{
+    private boolean[][] moveableTiles;
+
     public Pilot(String role, ArrayList<String> startingDeck) throws FileNotFoundException {
         super(role,startingDeck);
+        moveableTiles = new boolean[6][6];
+    }
+
+
+    public boolean[][] getMoveableTiles(){
+        for(int r = 0; r < 6; r++)
+        {
+            for(int c = 0; c < 6; c++)
+            {
+                moveableTiles[r][c] = true;
+
+            }
+        }
+        return moveableTiles;
     }
 }
