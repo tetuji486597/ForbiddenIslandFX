762d6e214ec439c3384e299c4e1d9d4d0f5590d7
yuiy0311 <luckykitty0311@gmail.com>
2021-11-26 20:31:05 -0600

    navigator.java edit getMoveableTiles

--- src/main/java/game/simulation/player/Navigator.java
+++ src/main/java/game/simulation/player/Navigator.java
@@ -24,8 +24,7 @@ public class Navigator extends Player{
             for(int c = 0; c < 3; c++)
             {
 
-                if(r == 1 && c == 1 || GameState.tiles[i].isGone() || r == 0 && c == 0 || r == 0 && c == 2 ||
-                        r == 2 && c == 0 || r == 2 && c == 2)
+                if(r == 1 && c == 1 || GameState.tiles[i].isGone() || r == 0 && c != 1 || r == 2 && c!= 1)
                 {
                     moveableTiles[r][c] = false;
                 }
