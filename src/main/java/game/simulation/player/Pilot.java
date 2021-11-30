334463f03990fa6d4f7a3d583d012a77eaf2ba3d
yuiy0311 <luckykitty0311@gmail.com>
2021-11-30 11:46:13 -0600

    Pilot.java getMoveabeTiles and boolean specialMove

--- src/main/java/game/simulation/player/Pilot.java
+++ src/main/java/game/simulation/player/Pilot.java
@@ -10,30 +10,58 @@ public class Pilot extends Player{
     private boolean[][] moveableTiles;
     private int[] moveTo;
     private boolean[][] shoreableTiles;
+    private boolean specialMove;
 
 
 
     public Pilot(String role, ArrayList<String> startingDeck) throws FileNotFoundException {
         super(role,startingDeck);
         moveableTiles = new boolean[6][6];
+        shoreableTiles = new boolean[6][6];
+        specialMove = false;
+    }
+
+    public void setSpecialMove(int[] oldPos, int[] newPos) {
+        if(oldPos[0] == newPos[0]+1 && oldPos[1] == newPos[1] || oldPos[0] == newPos[0] && oldPos[1] == newPos[1] -1 ||
+        oldPos[0] == newPos[0] && oldPos[1] == newPos[1] +1 || oldPos[0] == newPos[0] -1 && oldPos[1] == newPos[1])
+            specialMove = false;
+        else
+            specialMove = true;
     }
 
     @Override
     public boolean[][] getMoveableTiles(GameTile gameTile){
         char[][] board = GameState.getCurrentState();
-        for(int r = 0; r < 6; r++)
-        {
-            for(int c = 0; c < 6; c++)
-            {
-                if(board[r][c] == 'S')
-                    moveableTiles[r][c] = false;
+        if(specialMove){
+            int[] pos = tile.getPosition();
+            for (int r = 0; r < 6; r++) {
+                for (int c = 0; c < 6; c++) {
+                    if(board[r][c] == 'S' || r == pos[0] && c == pos[1])
+                        moveableTiles[r][c] = false;
+                    else if(r == pos[0]-1 && c == pos[1] || r == pos[0] && c == pos[1]-1 || r == pos[0] && c == pos[1]+1
+                            || r == pos[0] +1 && c == pos[1]){
+                        moveableTiles[r][c] = true;
+                    }
+                    else moveableTiles[r][c] = false;
 
-                else moveableTiles[r][c] = true;
+                }
             }
+            return moveableTiles;
+
         }
-        int pos[] = gameTile.getPosition();
-        moveableTiles[pos[0]][pos[1]] = false;
-        return moveableTiles;
+        else {
+            for (int r = 0; r < 6; r++) {
+                for (int c = 0; c < 6; c++) {
+                    if (board[r][c] == 'S')
+                        moveableTiles[r][c] = false;
+
+                    else moveableTiles[r][c] = true;
+                }
+            }
+            int pos[] = gameTile.getPosition();
+            moveableTiles[pos[0]][pos[1]] = false;
+            return moveableTiles;
+        }
     }
 
     public void movePawn(GameTile tile, int[] coords, Player p)
