e08db5b9c8f4a387d3b2eeac895eee224e9403e8
nitheeshcpu <nitishkoda@outlook.com>
2021-12-05 22:36:17 -0600

    big progress

--- src/main/java/game/graphics/GameBoardController.java
+++ src/main/java/game/graphics/GameBoardController.java
@@ -1624,7 +1624,7 @@ public class GameBoardController {
                 if(moveableTiles[r][c]) possibleToSwim = true;
             }
         }
-        if(!possibleToSwim) System.out.println("GAME OVER!!");
+        if(!possibleToSwim) GameState.loseGame("Player Drowned!");
         ImageView[] imageViews = new ImageView[]{r0c2,r0c3,r1c1,r1c2,r1c3,r1c4,r2c0,r2c1,r2c2,r2c3,r2c4,r2c5,r3c0,r3c1,r3c2,r3c3,r3c4,r3c5,r4c1,r4c2,r4c3,r4c4,r5c2,r5c3};
         for (ImageView im : imageViews)
             im.setEffect(null);
@@ -1685,7 +1685,7 @@ public class GameBoardController {
             try {
                 im.setVisible(false);
             }catch (ArrayIndexOutOfBoundsException ex){
-                System.out.println("GAME OVER!!!");
+                GameState.loseGame("The Island Sank");
             }
         }
         waterlevels[GameState.waterLevel-1].setVisible(true);
